f3452a17aef5d80db3fe6d307da1293951885626
吴海兵 <kaihei@wuhaibings-MacBook-Pro.local>
2021-01-30 23:30:30 +0800

    无重复支付穿最大长度

---
+++ start/无重复字符串最大长度.js
@@ -0,0 +1,30 @@
+// var lengthOfLongestSubstring = function(s) {
+//     let arr = []
+//     let max = 0
+//    for (let i = 0; i < s.length; i ++) {
+//        // 使用indexOf判断是否在数组中出现过
+//        let index = arrindexOf(s[i])
+//        if (index !== -1) {
+//            // 截去开头到当前字符串
+//            arr.splice(0, index + 1)
+//        }
+//        // 放入剩下的字符串到数组
+//        arr.push(s.charAt(i))
+//        // 取max最大值
+//        max = Math.max(arr.length, max)
+//    }
+//    return max
+// }
+
+var lengthOfLongestSubstring = function (s) {
+  let map = new Map(),
+    max = 0;
+  for (let i = 0, j = 0; j < s.length; j++) {
+    if (map.has(s[j])) {
+      i = Math.max(map.get(s[j]) + 1, i);
+    }
+    max = Math.max(max, j - i + 1);
+    map.set(s[j], j);
+  }
+  return max;
+};
